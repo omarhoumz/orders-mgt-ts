22e04ca8845238808bb6066e0a2702e574edcadb
Omar Houmz <omarhoumz@gmail.com>
2021-03-29 10:23:47 +0100

    chore: wip

--- pages/api/manager-restaurants.ts
+++ pages/api/manager-restaurants.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 import admin from '@/lib/firebase-admin'
-import { getManagerRestaurants } from '@/lib/db-admin'
+import { getManagerRestaurants, getMenuItems } from '@/lib/db-admin'
 
 const auth = admin.auth()
 
@@ -11,7 +11,7 @@ export default async function handler(
 ) {
   const { uid } = await auth.verifyIdToken(req.headers.token as string)
 
-  const restos = await getManagerRestaurants(uid)
+  const restos = await getMenuItems({ rid: 'xnzqLBJCHX1Nj76IDMX8' })
 
   res.status(200).json(restos)
 }
